68d40146bb2679dc7ca9ab0f19226df5a6b09340
Gustavo <ofoxsmith@outlook.com>
2025-01-03 11:48:47 +1100

    add type helpers for card class

--- cards.lua
+++ cards.lua
@@ -1,24 +1,37 @@
 dofile("utils.lua")
+require 'globals'
 
---[[
+---@alias Card BaseCard|AttackCard|DefenseCard|ModifierCard
 
-types
-- attack: has a projectile table
-- defense: 
-- self:
-- modifier: has a modifier table, a func_on_used, and a func_every_frame, use next > 0
-- multicast: use_next > 1
+---@class BaseCard
+---@field id string
+---@field name string
+---@field desc string
+---@field type CardType
+---@field sprite string,
+---@field cooldown integer
+---@field use_next integer
 
-damage types: projectile, slice, fire, poison, electricity
+---@class AttackCard: BaseCard
+---@field projectile table
 
-]]
+---@class DefenseCard: BaseCard
 
+---@class ModifierCard: BaseCard
+---@field modifier table,
+---@field func_on_used fun()
+---@field func_every_frame fun()
+
+---@class MulticastCard: BaseCard
+
+
+---@type Card[]
 cards_all = {
     {
         id = "bullet",
         name = "Bullet",
         desc = "Shoot a projectile",
-        type = "attack",
+        type = CardType.ATTACK,
         sprite = "gfx/cards/bullet.png",
         cooldown = 1, -- 1 turn cooldown, cooldowns are reduced at start of turn
         use_next = 0,
@@ -30,11 +43,20 @@ cards_all = {
     },
 }
 
+---@type Card[]
 cards_deck_player = {} -- between 10 and 20 cards
+
+---@type Card[]
 cards_deck_enemy = {}
 
+---@type Card[]
 cards_hand_player = {} -- 5 cards
+
+---@type Card[]
 cards_hand_enemy = {}
 
+---@type Card[]
 cards_barrel_player = {} -- up to 5, this is what gets 'shot' at the end of the turn
+
+---@type Card[]
 cards_barrel_enemy = {}
---
+++ globals.lua
@@ -0,0 +1,19 @@
+
+-- Enums
+
+---@enum CardType
+CardType = {
+    ATTACK = 1,
+    DEFENCE = 2,
+    MODIFIER = 3,
+    MULTICAST = 4
+}
+
+---@enum DamageType
+DamageType = {
+    PROJECTILE = 1,
+    SLICE = 2,
+    FIRE = 3,
+    POISON = 4,
+    ELECTRICITY = 5
+}
